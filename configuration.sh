23566d2a3db3c7ef48a72f9f767967f7b78b99b2
Mohsin riaz <l1f21bsse0473@ucp.edu.pk>
2025-01-20 09:05:08 +0500

    initial commit

---
+++ configuration.sh
@@ -0,0 +1,8 @@
+echo "Repo initial congigration Started"
+git branch -m "develop"
+git config --local user.name "Mohsin riaz"
+git config --local user.email "l1f21bsse0473@ucp.edu.pk"
+git config --local core.editore notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
+npm init -y
